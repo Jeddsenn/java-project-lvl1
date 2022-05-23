a4773c5661579b8d31fce74207b72f3a6dd44dca
Vladislav.P <vladppk@protonmail.com>
2022-05-23 13:19:14 +0200

    -Cli fix,

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void getTheName() {
+    public static String getTheName() {
         System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
+        return userName;
     }
 }
 
